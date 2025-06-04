968a5df342d1ff5e7568d7285eb577815aac2afe
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-06-03 22:59:14 -0400

    Background image and image to move added and display now

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -36,12 +36,14 @@ int main()
         al_destroy_timer(timer);
         return -1;
     }
+    al_init_image_addon();
     if (!al_init_image_addon()) {
         al_show_native_message_box(NULL, "Error", "Display failed to initialize", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
         return -1;
     }
     image = al_load_bitmap("forest.jpg");
     mikasa = al_load_bitmap("Mikasa.png");
+    event_queue = al_create_event_queue();
     if (!event_queue) {
         al_destroy_bitmap(mikasa);
         al_destroy_display(display);
@@ -84,13 +86,3 @@ int main()
     al_destroy_bitmap(image);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
