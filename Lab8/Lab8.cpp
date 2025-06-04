5d635d6fd2c416cbce1451c629801684aa378d3d
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-06-03 22:50:57 -0400

    Added event queue, error handling, and basic game loop

--- Lab8/Lab8.cpp
+++ Lab8/Lab8.cpp
@@ -4,25 +4,84 @@
 #include <iostream>
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
 
 int main()
 {
     const float FPS = 60;
     int width = 900, height = 800;
     const int mikasaSize = 32;
+    bool done = false, redraw = false;
     ALLEGRO_DISPLAY* display = NULL;
     ALLEGRO_EVENT_QUEUE* event_queue = NULL;
     ALLEGRO_TIMER* timer = NULL;
     ALLEGRO_BITMAP* mikasa = NULL;
+    float mikasa_x = width / 2.0 - mikasaSize / 2.0;
+    float mikasa_y = height / 2.0 - mikasaSize / 2.0;
+    float mikasa_dx = -4.0, mikasa_dy = 4.0;
+    ALLEGRO_BITMAP* image = NULL;
 
     if (!al_init()) {
+        al_show_native_message_box(NULL, "Error", "Allegro failed to initialize", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+    timer = al_create_timer(1.0 / FPS);
+    if (!timer) {
+        al_show_native_message_box(NULL, "Error", "Timer failed to initialize", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+    display = al_create_display(width, height);
+    if (!display) {
+        al_show_native_message_box(NULL, "Error", "Display failed to initialize", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        al_destroy_timer(timer);
+        return -1;
+    }
+    if (!al_init_image_addon()) {
+        al_show_native_message_box(NULL, "Error", "Display failed to initialize", 0, 0, ALLEGRO_MESSAGEBOX_ERROR);
+        return -1;
+    }
+    image = al_load_bitmap("forest.jpg");
+    mikasa = al_load_bitmap("Mikasa.png");
+    if (!event_queue) {
+        al_destroy_bitmap(mikasa);
+        al_destroy_display(display);
+        al_destroy_timer(timer);
+        return -1;
+    }
+    al_register_event_source(event_queue, al_get_display_event_source(display));
+    al_register_event_source(event_queue, al_get_timer_event_source(timer));
+    al_clear_to_color(al_map_rgb(0, 0, 0));
+    al_draw_scaled_bitmap(image, 0, 0, 1280, 720, 0, 0, width, height, 0);
+    al_draw_scaled_bitmap(mikasa, 0, 0, 840, 859, 0, 0, mikasa_x, mikasa_y, 0);
+    al_flip_display();
+    al_start_timer(timer);
 
+    while (!done) {
+        ALLEGRO_EVENT ev;
+        al_wait_for_event(event_queue, &ev);
+
+        if (ev.type == ALLEGRO_EVENT_TIMER) {
+            redraw = true;
+        }
+        else if (ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
+        {
+            done = true;
+        }
+        else if (redraw && al_is_event_queue_empty(event_queue)) {
+            redraw = false;
+
+            al_clear_to_color(al_map_rgb(0, 0, 0));
+            al_draw_scaled_bitmap(image, 0, 0, 1280, 720, 0, 0, width, height, 0);
+            al_draw_scaled_bitmap(mikasa, 0, 0, 840, 859, 0, 0, mikasa_x, mikasa_y, 0);
+            al_flip_display();
+        }
     }
 
     al_destroy_display(display);
     al_destroy_event_queue(event_queue);
     al_destroy_timer(timer);
     al_destroy_bitmap(mikasa);
+    al_destroy_bitmap(image);
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
